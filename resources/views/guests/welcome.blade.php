5aeb4d5863f109fda51b2a6ddc1072780686cb6b
MassimoRomano <romano93m@gmail.com>
2024-05-10 17:30:04 +0200

    add structure welcome for ticket

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -1,6 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-    
-
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="d-flex flex-column">
+                        <div class="card rounded-0 p-4">
+                            <div class="d-flex flex-row align-items-center justify-content-between">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="card rounded-0">
+                            <div class="d-flex flex-row align-items-center">
+                                <div class="col-3">
+                                    <div class="card m-2 p-3">
+                                        <h4 class="card-title">Partenza</h4>
+                                        <h5 class="card-text">milano</h5>
+                                        <p class="card-text">14:00</p>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="card m-2 p-3">
+                                        <h4 class="card-title">Arrivo</h4>
+                                        <h5 class="card-text">milano</h5>
+                                        <p class="card-text">14:00</p>
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="card m-2 p-3">
+                                        <h4 class="card-title">Arrivo</h4>
+                                        <h5 class="card-text">milano</h5>
+                                        <p class="card-text">14:00</p>
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="card m-2 p-3">
+                                        <h4 class="card-title">Arrivo</h4>
+                                        <h5 class="card-text">milano</h5>
+                                        <p class="card-text">14:00</p>
+                                    </div>
+                                </div>
+                                <div class="col-2">
+                                    <div class="card m-2 p-3">
+                                        <h4 class="card-title">Arrivo</h4>
+                                        <h5 class="card-text">milano</h5>
+                                        <p class="card-text">14:00</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
